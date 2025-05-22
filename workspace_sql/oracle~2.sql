c359890f96664e610f2dbf05ae59239caccf69b5
DESKTOP-UIAD7G2\admin <datascience123@naver.com>
2025-05-22 17:30:34 +0900

    no message

--- workspace_sql/oracle~2.sql
+++ workspace_sql/oracle~2.sql
@@ -907,3 +907,25 @@ select * from emp_hw;
 
 alter table emp_hw
     rename column bigo to remark;
+    
+select sal, deptno, sal + deptno from emp;
+
+select ascii('A') from dual;
+
+select * from emp;
+select * from emp
+where hiredate in to_date('1981-02', 'yyyy-mm')  ; --아무것도 출력되지 않는다 .
+
+select to_char(to_date('2023.01.01 10','yyyy.mm.dd hh24') + 
+    1/24/(60/10),'yyyy.mm.dd hh24:mi:ss') 
+    from dual;
+
+select * from dept;    
+--select 
+--    loc,     
+--    case 
+--        when loc is 'NEW YORK' 
+--        then 'east' 
+--        else 'etc' 
+--        end as area
+--        from dept;
