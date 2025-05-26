229990f003b50a6d7afe5687538db938d8aba123
DESKTOP-UIAD7G2\admin <datascience123@naver.com>
2025-05-26 15:30:05 +0900

    자바 수업 첫날

---
+++ workspace_java/chap00/src/sec03/exam01/Hello.java
@@ -0,0 +1,24 @@
+package sec03.exam01;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System
+		.out
+		.println
+		("Hello "
+					+ "World");
+
+		// 한줄 주석 //
+
+		/*
+		 * for (int x = 1; x < 10; x++) { for (int y = 1; y < x; y++) {
+		 * System.out.println("*"); } System.out.println(); }
+		 */
+
+		// ← 이 괄호는 반드시 주석 바깥에 있어야 함
+
+	}
+}
---
+++ workspace_java/chat01/src/sec01/exam01/VarExam.java
@@ -0,0 +1,17 @@
+package sec01.exam01;
+
+public class VarExam {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int value=0;
+		int result = value + 10;
+		
+		System.out.println("결과  : " + result);
+		System.out.println("결과  : " + result);
+		System.out.println("결과  : " + result);
+		System.out.println("결과  : " + result);
+		//ctrl + alt + 위아래 방향키 -> 복사, 붙혀널기
+	}
+
+}
