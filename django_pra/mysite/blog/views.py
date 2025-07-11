f804fccb6ea6e256975c4ff559db11c090da46f5
DESKTOP-UIAD7G2\admin <datascience123@naver.com>
2025-07-11 17:57:21 +0900

    잔고 뷰 생성. url이 왜 저렇게 되지?

--- django_pra/mysite/blog/views.py
+++ django_pra/mysite/blog/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from .models import Post #.은 경로 지정
+from django.http import Http404
 
 # Create your views here.
+
+def post_detail(request,id):
+    try :
+        post = Post.published.get(id=id)
+    
+    except Post.DoesNotExist :
+        raise Http404("NO POST FOUND")
+    
+    return render(request, 'blog/post/detail.html',{"post" : post})
+
+def post_list(request):
+    posts= Post.published.all()
+    return render(request, "blog/post/list.html", {"posts" : posts})
