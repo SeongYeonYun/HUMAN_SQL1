7c64284ba089532f3e89ae93d306a1e1b137ca4a
DESKTOP-UIAD7G2\admin <datascience123@naver.com>
2025-07-16 17:30:56 +0900

    why occured 404error????

--- django_pra/mysite/blog/views.py
+++ django_pra/mysite/blog/views.py
@@ -5,14 +5,19 @@ from django.http import Http404
 # Create your views here.
 
 def post_detail(request,id):
+    
     try :
         post = Post.published.get(id=id)
     
     except Post.DoesNotExist :
         raise Http404("NO POST FOUND")
     
+    
+    post = get_object_or_404(Post, id = id , status = Post.Status.PUBLISHED)
     return render(request, 'blog/post/detail.html',{"post" : post})
+    # return render(request, 'blog/post/14-3_6번_Todo.html',{"post" : post})
 
 def post_list(request):
     posts= Post.published.all()
     return render(request, "blog/post/list.html", {"posts" : posts})
+    # return render(request, 'blog/post/14-3_6번_Todo.html',{"post" : post})
