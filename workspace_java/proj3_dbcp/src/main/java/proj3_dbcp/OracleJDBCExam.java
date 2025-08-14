89d675d493d8bfd30ee33ecfb71075c611991ec7
DESKTOP-UIAD7G2\admin <datascience123@naver.com>
2025-08-14 17:26:09 +0900

    JDBC기본 살장

--- workspace_java/proj3_dbcp/src/main/java/proj3_dbcp/OracleJDBCExam.java
+++ workspace_java/proj3_dbcp/src/main/java/proj3_dbcp/OracleJDBCExam.java
@@ -41,45 +41,45 @@ public class OracleJDBCExam {
 		}
 		
 		
-		try {
-			
-			//객체 생성
-			Statement stmt = conn.createStatement();
-			
-			//내가 원하는 sql문 실행하는법
-			ResultSet rs = stmt.executeQuery("SELECT * FROM EMP");
-		
-			//메타 데이터를 가져와서
-			ResultSetMetaData meta = rs.getMetaData();
-			System.out.println("meta = " + meta);
-			System.out.println("테이블명: " + meta.getTableName(1));
-			System.out.println("스키마명: " + meta.getSchemaName(1));
-			System.out.println("카탈로그명: " + meta.getCatalogName(1));
-			
-			//메타 데이터의 칼럼수 조회
-			int columnCount = meta.getColumnCount();
-			System.out.println("columnCount = " + columnCount);
-
-			//칼럼 수만큼 반복해서 가져오기
-			while (rs.next()) {
-			    for (int i = 1; i <= columnCount; i++) {
-			    	
-			    	//getString -> 데이터를 스트링 값으로 가져온다
-			        System.out.print(rs.getString(i) + "\t");
-			    }
-			    System.out.println();
-			}
-			
-			rs.close();
-			stmt.close();
-			conn.close();
-			System.out.println("\n--------데이터 베이스 접속 종료----------");
-			
-			
-		} catch (Exception e) {
-			e.printStackTrace();
-			
-		}
+//		try {
+//			
+//			//객체 생성
+//			Statement stmt = conn.createStatement();
+//			
+//			//내가 원하는 sql문 실행하는법
+//			ResultSet rs = stmt.executeQuery("SELECT * FROM EMP");
+//		
+//			//메타 데이터를 가져와서
+//			ResultSetMetaData meta = rs.getMetaData();
+//			System.out.println("meta = " + meta);
+//			System.out.println("테이블명: " + meta.getTableName(1));
+//			System.out.println("스키마명: " + meta.getSchemaName(1));
+//			System.out.println("카탈로그명: " + meta.getCatalogName(1));
+//			
+//			//메타 데이터의 칼럼수 조회
+//			int columnCount = meta.getColumnCount();
+//			System.out.println("columnCount = " + columnCount);
+//
+//			//칼럼 수만큼 반복해서 가져오기
+//			while (rs.next()) {
+//			    for (int i = 1; i <= columnCount; i++) {
+//			    	
+//			    	//getString -> 데이터를 스트링 값으로 가져온다
+//			        System.out.print(rs.getString(i) + "\t");
+//			    }
+//			    System.out.println();
+//			}
+//			
+//			rs.close();
+//			stmt.close();
+//			conn.close();
+//			System.out.println("\n--------데이터 베이스 접속 종료----------");
+//			
+//			
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//			
+//		}
 
 		
 		
