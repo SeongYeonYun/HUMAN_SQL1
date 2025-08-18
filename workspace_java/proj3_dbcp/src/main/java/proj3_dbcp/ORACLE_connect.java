1fda1d5b49a82906f343efc3aaa278bea492aa4b
DESKTOP-UIAD7G2\admin <datascience123@naver.com>
2025-08-18 11:38:02 +0900

    no message

---
+++ workspace_java/proj3_dbcp/src/main/java/proj3_dbcp/ORACLE_connect.java
@@ -0,0 +1,15 @@
+package proj3_dbcp;
+
+import java.sql.Connection;
+//import java.sql.DriverManager;
+
+public class ORACLE_connect {
+	// TODO Auto-generated method stub
+		String url = "jdbc:oracle:thin:@125.181.132.133:51521:xe"; 
+		String user = "scott4_12";
+		String pw = "tiger";
+		
+		
+		}
+
+
