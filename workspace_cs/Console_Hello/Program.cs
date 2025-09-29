a9e2b67d9c516445c9699780740c3f6c1604f91c
DESKTOP-UIAD7G2\admin <datascience123@naver.com>
2025-09-29 09:01:32 +0900

    c#

---
+++ workspace_cs/Console_Hello/Program.cs
@@ -0,0 +1,29 @@
+﻿
+
+
+class Program { 
+    public static void Main() { 
+        //System.Console.WriteLine("Hello, World!");
+        //System.Console.Write("Hello, World!");
+
+
+        int[] arr = [1, 2, 3, 4, 5];
+
+        for (int i = 0; i < 10; i++) {
+            System.Console.WriteLine(i * arr[1]);
+
+
+        }
+
+        test();
+
+    }
+
+    static void test() {
+        System.Console.WriteLine("test");
+    }
+
+
+}
+
+
